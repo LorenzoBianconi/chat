e056a869c7cfcd81ed8bf2ee078fdaed75f3292e
lorenzo bianconi <lorenzo@kxps>
2012-10-16 12:58:06 +0200

    modified Copyright

--- server.c
+++ server.c
@@ -1,5 +1,5 @@
 /*
- * Copyright 2011-2012	Lorenzo Bianconi <me@lorenzobianconi.net>
+ * Copyright 2011-2013	Lorenzo Bianconi <me@lorenzobianconi.net>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 as
